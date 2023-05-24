8bf355459733d98c863ba226b22afb9d437eeed9
venkatajagadeesh <venkatajagadeesh.jami@gmail.com>
2023-05-24 14:54:39 +0530

    Added @EnableEncryptableProperties for encrypable

--- passenger_api/src/main/java/com/passenger/PassengerApiApplication.java
+++ passenger_api/src/main/java/com/passenger/PassengerApiApplication.java
@@ -5,13 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
+@EnableEncryptableProperties
 @SpringBootApplication
 public class PassengerApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PassengerApiApplication.class, args);
 	}
-	
+
 	@Bean
 	public ModelMapper getModelMapper() {
 		return new ModelMapper();
